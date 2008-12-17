5a3ee0ca89e348ba2fad6b854d2a5ee6a6a0cc6a
Väinö Järvelä <vaino.jarvela@gmail.com>
2008-12-17 12:11:45 +0200

    Use UTF-8 everywhere
    UTF-8 characters were not displayed before this change

--- kaali.el
+++ kaali.el
@@ -260,7 +260,14 @@
 (column-number-mode t)
 (cua-mode t)
 (setq cua-enable-cua-keys nil)
+
 (setq current-language-environment "UTF-8")
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 (setq transient-mark-mode t)
 (show-paren-mode t)
 (size-indication-mode t)

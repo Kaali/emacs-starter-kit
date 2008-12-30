1a7f331bf4f37bb8b9ff68c48ba4b0ee2462b189
Väinö Järvelä <vaino.jarvela@gmail.com>
2008-12-30 09:22:40 +0200

    Removed some of my user-config UTF-8 lines
    They are now also defined in starter-kit-misc.el

--- kaali.el
+++ kaali.el
@@ -263,10 +263,7 @@
 
 (setq current-language-environment "UTF-8")
 (setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
 
 (setq transient-mark-mode t)
 (show-paren-mode t)

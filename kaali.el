f9dfcc78884edaa4f4e2d5832ff8d67afff276bc
Väinö Järvelä <vaino.jarvela@gmail.com>
2009-03-15 21:51:14 +0200

    Added haskell-mode

--- kaali.el
+++ kaali.el
@@ -165,6 +165,13 @@
 (global-whitespace-mode t)
 
 
+;; Haskell-mode
+(load (concat vendor-dir "haskell-mode-2.4/haskell-site-file"))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
+
 ;;
 ;; Key bindings / input
 ;;

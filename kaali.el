7ded9c159cc8e2f249329c9d29e29db1eb729658
Väinö Järvelä <vaino.jarvela@gmail.com>
2009-03-15 21:52:21 +0200

    Added smart-split frame splitter

--- kaali.el
+++ kaali.el
@@ -338,6 +338,27 @@
 ;; Mark-ring is navigable by typing C-u C-SPC and then repeating C-SPC forever
 (setq set-mark-command-repeat-pop t)
 
+
+;; From http://hjiang.net/archives/253, optional arg added by
+;; Väinö Järvelä
+(defun smart-split (&optional arg)
+  "Split the frame into 80-column sub-windows, and make sure no window has
+   fewer than 80 columns.
+
+   If OPTIONAL arg is supplied, then use that as the minimum width of the
+   windows."
+  (interactive "P")
+  (defun smart-split-helper (w min-width)
+    "Helper function to split a given window into two, the first of which
+     has min-width columns."
+    (if (> (window-width w) (* 2 (+ min-width 1)))
+    (let ((w2 (split-window w (+ min-width 2) t)))
+      (smart-split-helper w2 min-width))))
+  (smart-split-helper nil (or arg 80)))
+
+(define-key global-map (kbd "C-c s") 'smart-split)
+
+
 ;; Some settings
 (column-number-mode t)
 (cua-mode t)

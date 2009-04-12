818b00b4975fe4485ddfe07fd64f7a805c114f83
Väinö Järvelä <vaino.jarvela@gmail.com>
2009-04-12 16:26:58 +0300

    Replaced flymake-cursors with a manual binding
    It had some conflicts with other Emacs features and packages, such as anything.el

--- kaali.el
+++ kaali.el
@@ -39,7 +39,27 @@
 ;;
 
 ;; Flymake-mode
-(load-library "flymake-cursor")
+
+(defun credmp/flymake-display-err-minibuf ()
+  "Displays the error/warning for the current line in the minibuffer"
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+         (count               (length line-err-info-list))
+         )
+    (while (> count 0)
+      (when line-err-info-list
+        (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+               (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
+               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
+               (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+          (message "[%s] %s" line text)
+          )
+        )
+      (setq count (1- count)))))
+(define-key global-map (kbd "C-c ;") 'credmp/flymake-display-err-minibuf)
+
+
 (require 'flymake)
 (set-face-attribute 'flymake-warnline nil
                     :background zenburn-bg
